4f08b5c536227c9cc5ac885831080ddf93fc8eb3
kingk4 <92218590+kingk4@users.noreply.github.com>
2022-10-21 16:13:31 +0500

    Update Updated-selenium-Chromedriver.py

--- Updated-selenium-Chromedriver.py
+++ Updated-selenium-Chromedriver.py
@@ -4,7 +4,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
 s = Service('C:/chromedriver.exe')
-driver = webdriver.Chrome(service=s)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])          # keep automation
+chrome_options.add_experimental_option('useAutomationExtension', False)              # bot hidden on web
+chrome_options.add_experimental_option("detach", True)        
+chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+chrome_options.add_argument("--incognito")                              # keep active browser
+prefs = {"profile.default_content_setting_values.notifications" : 2}
+chrome_options.add_experimental_option("prefs", prefs)
+driver = webdriver.Chrome(chrome_options=chrome_options, service= s)
+driver.implicitly_wait(5)
+driver.maximize_window()
 
 url = 'https://the-internet.herokuapp.com/login'
 
